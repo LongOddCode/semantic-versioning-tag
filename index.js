1a98f6fada2c942572111ff7d44c949ec9870466
Long Hao <haolong@microsoft.com>
2021-03-19 10:49:05 +0800

    update main

--- index.js
+++ index.js
@@ -3,17 +3,19 @@ const github = require("@actions/github");
 
 const regexStr = /^(?<major>0|[1-9]\d*)\.(?<minor>0|[1-9]\d*)\.(?<patch>0|[1-9]\d*)(?:-(?<prerelease>(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+(?<buildmetadata>[0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?$/gm;
 
+const regex = new RegExp(regexStr);
 const ref = github.context.ref;
 const tagPath = "refs/tags/";
 if (ref && ref.startsWith(tagPath)) {
-    var regex = new RegExp(regexStr);
+    var tag = ref.substr(tagPath.length, ref.length);
     var result = regex.exec(tag);
     if (!result) {
         core.setFailed("not semantic versionning tag");
+    } else {
+        core.setOutput("major", result["major"]);
+        core.setOutput("minor", result["minor"]);
+        core.setOutput("patch", result["patch"]);
+        core.setOutput("prerelease", result["prerelease"]);
+        core.setOutput("buildmetadata", result["buildmetadata"]);
     }
-    core.setOutput("major", result["major"]);
-    core.setOutput("minor", result["minor"]);
-    core.setOutput("patch", result["patch"]);
-    core.setOutput("prerelease", result["prerelease"]);
-    core.setOutput("buildmetadata", result["buildmetadata"]);
 }
