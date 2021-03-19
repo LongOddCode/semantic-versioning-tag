356b76aa61c0b53ab0cbf6d1d2bf1eb6ff3da4ca
Long Hao <haolong@microsoft.com>
2021-03-19 15:00:01 +0800

    update main

--- index.js
+++ index.js
@@ -7,21 +7,18 @@ const regex = new RegExp(regexStr);
 
 const ref = github.context.ref;
 const tagPath = "refs/tags/";
-console.log(ref);
-console.log(tagPath);
 
 if (ref && ref.startsWith(tagPath)) {
     var tag = ref.substr(tagPath.length, ref.length);
-    console.log(tag);
     var result = regex.exec(tag);
     if (!result) {
         core.setFailed("not semantic versionning tag");
     } else {
-        console.log(result);
-        core.setOutput("major", result["major"]);
-        core.setOutput("minor", result["minor"]);
-        core.setOutput("patch", result["patch"]);
-        core.setOutput("prerelease", result["prerelease"]);
-        core.setOutput("buildmetadata", result["buildmetadata"]);
+        console.log(result.groups);
+        core.setOutput("major", result.groups["major"]);
+        core.setOutput("minor", result.groups["minor"]);
+        core.setOutput("patch", result.groups["patch"]);
+        core.setOutput("prerelease", result.groups["prerelease"]);
+        core.setOutput("buildmetadata", result.groups["buildmetadata"]);
     }
 }
